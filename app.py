eceea1933db6a5bcfd8611ca6a4c97569d5fa4ae
Kauê Miranda <kauemiranda7966@gmail.com>
2026-04-30 11:03:45 -0300

    adicionando imagens

--- app.py
+++ app.py
@@ -3,38 +3,6 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-
-import sqlite3
-
-def conectar():
-    return sqlite3.connect("database.db")
-
-def criar_tabelas():
-    conn = conectar()
-    cursor = conn.cursor()
-
-    cursor.execute ("""
-    CREATE TABLE IF NOT EXISTS usuarios (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT,
-        senha TEXT
-    )
-    """)
-
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS gastos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT,
-        valor REAL,
-        usuario_id INTEGER,
-        FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
-    )
-    """)
-
-    conn.commit()
-    conn.close()
-
 from flask import Flask
 from routes.auth import auth
 from routes.gastos import gastos_bp
